1f771d30511ea47e5ee7a5cff8cc80fce95522c4
rakhat04 <akmyrzaevrakhatr@gmail.com>
2025-02-03 19:36:02 +0500

    peace of cake)))

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,36 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
 
     public static void main(String[] args) {
-        //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
         a = inputDouble();
         b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+        sum();
+        multiply();
+        diff();
+        divide();
     }
 
     private static void divide() {
-        //todo дописать логику и печать результата
+        System.out.println("Divide: " + a / b);
     }
 
     private static void diff() {
-        //todo дописать логику и печать результата
+        System.out.println("Diff: " + (a - b));
     }
 
     private static void multiply() {
-        //todo дописать логику и печать результата
+        System.out.println("Multiply: " + (a * b));
     }
 
     private static void sum() {
-        //todo дописать логику и печать результата
+        System.out.println("Sum: " + (a + b));
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        System.out.println("Print number to calculate: ");
+        return new Scanner(System.in).nextDouble();
     }
 }
